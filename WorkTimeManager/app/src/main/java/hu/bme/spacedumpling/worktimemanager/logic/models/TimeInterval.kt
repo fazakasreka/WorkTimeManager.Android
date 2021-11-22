27152e0fad3ac0582be1a240e8df90aa3fbbc758
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2021-11-22 16:27:54 +0100

    cells

--- WorkTimeManager/app/src/main/java/hu/bme/spacedumpling/worktimemanager/logic/models/TimeInterval.kt
+++ WorkTimeManager/app/src/main/java/hu/bme/spacedumpling/worktimemanager/logic/models/TimeInterval.kt
@@ -8,5 +8,22 @@ data class TimeInterval (
     val startTime: Date? = null,
     val endTime: Date? = null,
 ){
+    fun validtate() : ValidationResult{
+        if(taskId == null) return ValidationResult.NO_TASK
+        if(startTime == null || endTime == null) return ValidationResult.NO_TIME
+        if(date == null) return ValidationResult.NO_DATE
+        if(startTime.after(endTime)){
+            return ValidationResult.START_TIME_AFTER_END_TIME
+        }
+        return ValidationResult.VALID
+    }
+
+    enum class ValidationResult{
+        VALID,
+        NO_DATE,
+        NO_TASK,
+        NO_TIME,
+        START_TIME_AFTER_END_TIME,
+    }
 
 }
--- WorkTimeManager/app/src/main/java/hu/bme/spacedumpling/worktimemanager/presentation/page/home/HomeViewModel.kt
+++ WorkTimeManager/app/src/main/java/hu/bme/spacedumpling/worktimemanager/presentation/page/home/HomeViewModel.kt
@@ -3,8 +3,10 @@ package hu.bme.spacedumpling.worktimemanager.presentation.page.home
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import hu.bme.spacedumpling.worktimemanager.logic.models.TasksByProject
+import hu.bme.spacedumpling.worktimemanager.logic.models.TimeInterval
 import hu.bme.spacedumpling.worktimemanager.logic.repository.home.HomeRepository
 import hu.bme.spacedumpling.worktimemanager.presentation.baseclasses.viewmodels.BaseViewModel
+import hu.bme.spacedumpling.worktimemanager.presentation.page.projects.MakeToast
 import hu.bme.spacedumpling.worktimemanager.presentation.page.projects.PageReloadRequest
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
@@ -25,7 +27,14 @@ class HomeViewModel(
                         _chosenProject.tryEmit(it.tasksByProject)
                     }
                     is SaveTimeInterval -> {
-                        //todo
+                        when(it.timeInterval.validtate()) {
+                            TimeInterval.ValidationResult.VALID -> {
+                                fragmentActionFlow.tryEmit(MakeToast("Saving..."))
+                            }
+                            TimeInterval.ValidationResult.NO_TASK -> {
+                                fragmentActionFlow.tryEmit(MakeToast("Choose task"))
+                            }
+                        }
                     }
                 }
             }
